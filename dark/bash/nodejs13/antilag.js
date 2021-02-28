54ce53dee02a6b126ace87c5f5b52550c6766d12
mateuzis <79773992+mateuzis@users.noreply.github.com>
2021-02-28 11:25:05 -0300

    Add files via upload

---
+++ dark/bash/nodejs13/antilag.js
@@ -0,0 +1,14 @@
+const antilag = (prefix) => { 
+	return `
+	
+	*Soluçao! Este recurso ainda está em desenvolvimento, aguarde a última atualização da versão Fox Bot*
+	
+[❗] *As últimas atualizações do WhatsApp Bot estão apenas no canal do Youtube 
+
+⧿⧽ *Não se esqueça de se inscrever*
+⧿⧽ *Ative os sinos*
+⧿⧽ *Atenciosamente Fox*
+
+`
+}
+exports.antilag = antilag
